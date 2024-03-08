92af82ced8d931af8cc8d6d1eacae468026e8eb2
Varun Maurya <varun19maurya@gmail.com>
2024-03-08 12:51:14 +0530

    Added block/unblock user

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,15 @@
 const express = require("express");
 
-const {createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updatedUser} = require("../controller/userCtrl");
+const {
+    createUser, 
+    loginUserCtrl, 
+    getallUser, 
+    getaUser, 
+    deleteaUser, 
+    updatedUser, 
+    blockUser, 
+    unblockUser
+} = require("../controller/userCtrl");
 const {authMiddleware, isAdmin} = require("../middlewares/authMiddleware")
 
 const router = express.Router();
@@ -8,7 +17,9 @@ router.post("/register",createUser);
 router.post("/login",loginUserCtrl);
 router.get('/all-users',getallUser);
 router.get('/:id', authMiddleware, isAdmin , getaUser);
-router.put('/edit-user',authMiddleware ,updatedUser);
 router.delete('/:id',deleteaUser);
+router.put('/edit-user',authMiddleware, updatedUser);
+router.put('/block-user/:id',authMiddleware, isAdmin, blockUser);
+router.put('/unblock-user/:id',authMiddleware, isAdmin, unblockUser);
 module.exports = router;
 
