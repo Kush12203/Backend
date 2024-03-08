97bf3600a4b3a1e4d266b3aa8b2420bcad0f9e07
Varun Maurya <varun19maurya@gmail.com>
2024-03-08 12:03:44 +0530

    Added authMiddleware

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -82,10 +82,10 @@ const deleteaUser = asyncHandler(async (req, res) => {
 
 
 const updatedUser = asyncHandler(async (req, res) => {
-  const { id } = req.params;
+  const { _id } = req.user;
   try {
     const updatedUser = await User.findByIdAndUpdate(
-      id, 
+      _id, 
       {
         firstname: req?.body?.firstname,
         lastname: req?.body?.lastname,
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,13 +1,14 @@
 const express = require("express");
 
 const {createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updatedUser} = require("../controller/userCtrl");
+const {authMiddleware, isAdmin} = require("../middlewares/authMiddleware")
 
 const router = express.Router();
 router.post("/register",createUser);
 router.post("/login",loginUserCtrl);
 router.get('/all-users',getallUser);
-router.get('/:id',getaUser);
+router.get('/:id', authMiddleware, isAdmin , getaUser);
+router.put('/edit-user',authMiddleware ,updatedUser);
 router.delete('/:id',deleteaUser);
-router.put('/:id',updatedUser);
 module.exports = router;
 
