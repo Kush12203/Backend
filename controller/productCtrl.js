823108b8d9907849543db6d22fdff7dd0f36f803
Varun Maurya <varun19maurya@gmail.com>
2024-03-12 02:59:53 +0530

    Added wishlist

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,4 +1,5 @@
 const Product = require("../models/productModel");
+const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
 
@@ -20,9 +21,13 @@ const updateProduct = asyncHandler(async (req, res) => {
         if (req.body.title) {
             req.body.slug = slugify(req.body.title);
         }
-        const updatedProduct = await Product.findOneAndUpdate({ _id: id }, req.body, {
-            new: true,
-        });
+        const updatedProduct = await Product.findOneAndUpdate(
+            { _id: id },
+            req.body,
+            {
+                new: true,
+            }
+        );
         res.json(updatedProduct);
     } catch (error) {
         throw new Error(error);
@@ -58,4 +63,47 @@ const getAllProduct = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct};
+const addToWishlist = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    const { prodId } = req.body;
+    try {
+        const user = await User.findById(_id);
+        const alreadyadded = user.wishlist.find(
+            (id) => id.toString() === prodId
+        );
+        if (alreadyadded) {
+            let user = await User.findByIdAndUpdate(
+                _id,
+                {
+                    $pull: { wishlist: prodId },
+                },
+                {
+                    new: true,
+                }
+            );
+            res.json(user);
+        } else {
+            let user = await User.findByIdAndUpdate(
+                _id,
+                {
+                    $push: { wishlist: prodId },
+                },
+                {
+                    new: true,
+                }
+            );
+            res.json(user);
+        }
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+module.exports = {
+    createProduct,
+    getaProduct,
+    getAllProduct,
+    updateProduct,
+    deleteProduct,
+    addToWishlist
+};
