6e7d0e95ce7896ed8774a6572cfa8b99141140dc
Krishnendu0016 <16kdas@gmail.com>
2024-03-16 21:01:02 +0530

    Error solved for sending mail

--- controller/emailCtrl.js
+++ controller/emailCtrl.js
@@ -1,33 +1,33 @@
-const nodemailer= require("nodemailer");
-const asyncHandler= require("express-async-handler");
+const nodemailer = require("nodemailer");
+const asyncHandler = require("express-async-handler");
 
-const sendEmail=asyncHandler(async(data,req,res)=>{
-    const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false, 
-        auth: {
-          user: process.env.MAIL_ID,
-          pass: process.env.MP,
-        },
+const sendEmail = asyncHandler(async (data, req, res) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.MAIL_ID,
+      pass: process.env.MP,
+    },
+  });
+
+  async function main() {
+    try {
+      const info = await transporter.sendMail({
+        from: '"Hey 👻" <abc@gmail.com>',
+        to: data.to,
+        subject: data.subject,
+        text: data.text,
+        html: data.htm,
       });
-      
-      async function main() {
-        try {
-          const info = await transporter.sendMail({
-            from: '"Hey 👻" <abc@gmail.com>', 
-            to: data.to, 
-            subject: data.subject, 
-            text: data.text, 
-            html: data.htm, 
-          });
-          console.log("Message sent: %s", info.messageId);
-        } catch (error) {
-          console.error("Error occurred while sending email: ", error);
-        }
-      }
-      
+      console.log("Message sent: %s", info.messageId);
+    } catch (error) {
+      console.error("Error occurred while sending email: ", error);
+    }
+  }
+  main();
 
 });
 
-module.exports=sendEmail;
+module.exports = sendEmail;
